a68555d1417710fd873b13f54f009684d16a8b75
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-22 16:23:16 -0400

    Update XFloats.jl

--- src/XFloats.jl
+++ src/XFloats.jl
@@ -154,8 +154,8 @@ end
 show(io::IO, x::XFloat16) = print(io, Float16(reinterpret(Float32, x)))
 show(io::IO, x::XFloat32) = print(io, Float32(reinterpret(Float64, x)))
 
-hash(x::XFloat16) = hash(reintepret(Float32, x))
-hash(x::XFloat32) = hash(reintepret(Float64, x))
+hash(x::XFloat16) = hash(reinterpret(Float32, x))
+hash(x::XFloat32) = hash(reinterpret(Float64, x))
 
 tryparse(::Type{XFloat16}, x::String) = reinterpret(XFloat16, tryparse(Float32, x))
 tryparse(::Type{XFloat32}, x::String) = reinterpret(XFloat32, tryparse(Float64, x))
