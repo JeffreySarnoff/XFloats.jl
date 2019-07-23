3019804d9be75f369fb9d68570da5e9b04f1a204
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-23 04:09:14 -0400

    Update XFloats.jl

--- src/XFloats.jl
+++ src/XFloats.jl
@@ -205,23 +205,23 @@ for (XT, FT) in ((:XFloat16, :Float32), (:XFloat32, :Float64))
   end
 
   for Op in UnaryVectorOps_oftype
-    @eval $Op(x::$XT) = reinterpret($XT, $Op(reinterpret($FT, x)))
+    @eval $Op(x::Vector{$XT}) = reinterpret($XT, $Op(reinterpret($FT, x)))
   end
   for Op in BinaryVectorOps_oftype
-    @eval $Op(x::$XT, y::$XT) = reinterpret($XT, $Op(reinterpret($FT, x), reinterpret($FT, y)))
+    @eval $Op(x::Vector{$XT}, y::Vector{$XT}) = reinterpret($XT, $Op(reinterpret($FT, x), reinterpret($FT, y)))
   end
   for Op in UnaryMatrixOps_oftype
-    @eval $Op(x::$XT) = reinterpret($XT, $Op(reinterpret($FT, x)))
+    @eval $Op(x::Matrix{$XT}) = reinterpret($XT, $Op(reinterpret($FT, x)))
   end
   for Op in BinaryMatrixOps_oftype
-    @eval $Op(x::$XT, y::$XT) = reinterpret($XT, $Op(reinterpret($FT, x), reinterpret($FT, y)))
+    @eval $Op(x::Matrix{$XT}, y::Matrix{$XT}) = reinterpret($XT, $Op(reinterpret($FT, x), reinterpret($FT, y)))
   end
 
   for Op in MatrixToVectorOps_oftype
-    @eval $Op(x::$XT) = reinterpret($XT, $Op(reinterpret($FT, x)))
+    @eval $Op(x::Matrix{$XT}) = reinterpret($XT, $Op(reinterpret($FT, x)))
   end
   for Op in MatrixToMatrixOps_oftype
-    @eval $Op(x::$XT) = reinterpret($XT, $Op(reinterpret($FT, x)))
+    @eval $Op(x::Matrix{$XT}) = reinterpret($XT, $Op(reinterpret($FT, x)))
   end
     
   inv(x::Array{XFloat16, N}) where {N} =
