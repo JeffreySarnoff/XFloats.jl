6529828ab4f701b74dae44e1e9506869a9d812cb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-24 17:11:26 -0400

    Create arithmetic.jl

---
+++ benchmarks/arithmetic.jl
@@ -0,0 +1,16 @@
+a32, a64 = Float32(pi)/7, Float64(pi)/7
+b32, b64 = Float32(cbrt(3)), Float64(cbrt(3))
+
+x16, x32 = XFloat16(a32), XFloat32(a64)
+y16, y32 = XFloat16(b32), XFloat32(b64)
+
+add_ab32 = @noelided @belapsed $a32 + $b32
+add_ab64 = @noelided @belapsed $a64 + $b64
+add_xy16 = @noelided @belapsed $x16 + $y16
+add_xy32 = @noelided @belapsed $x32 + $y32
+
+add_16 = string(round(add_xy16 / add_ab32, digits = 4)) 
+add_32 = string(round(add_xy32 / add_ab64, digits = 4)) 
+
+println("add_16 = $add_16")
+println("add_32 = $add_32")
