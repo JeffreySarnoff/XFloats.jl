4b09d843b2c2b29e268439192809eebbc0005d07
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-24 04:10:30 -0400

    Create runbenchmarks.jl

---
+++ benchmarks/runbenchmarks.jl
@@ -0,0 +1,7 @@
+using XFloats
+using BenchmarkTools
+const BT=BenchmarkTools.DEFAULT_PARAMETERS;
+BT.overhead = BenchmarkTools.estimate_overhead(); BT.evals=1; BT.time_tolerance = 2.0e-9; BT.samples = 300;
+
+include("noelide.jl")
+
