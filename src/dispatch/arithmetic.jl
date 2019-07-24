247b8593372345fe5934fbbb44cfb3738101be88
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-24 17:18:04 -0400

    Update arithmetic.jl

--- src/dispatch/arithmetic.jl
+++ src/dispatch/arithmetic.jl
@@ -1,1 +1,14 @@
+import Base: +, -, *, /, \
+
+(+)(x::XFloat16, y::XFloat16) = reinterpret(XFloat16, (+)(reinterpret(Float32, x), reinterpret(Float32, y)))
+(-)(x::XFloat16, y::XFloat16) = reinterpret(XFloat16, (-)(reinterpret(Float32, x), reinterpret(Float32, y)))
+(*)(x::XFloat16, y::XFloat16) = reinterpret(XFloat16, (*)(reinterpret(Float32, x), reinterpret(Float32, y)))
+(/)(x::XFloat16, y::XFloat16) = reinterpret(XFloat16, (/)(reinterpret(Float32, x), reinterpret(Float32, y)))
+(\)(x::XFloat16, y::XFloat16) = reinterpret(XFloat16, (\)(reinterpret(Float32, x), reinterpret(Float32, y)))
+
+(+)(x::XFloat32, y::XFloat32) = reinterpret(XFloat32, (+)(reinterpret(Float64, x), reinterpret(Float64, y)))
+(-)(x::XFloat32, y::XFloat32) = reinterpret(XFloat32, (-)(reinterpret(Float64, x), reinterpret(Float64, y)))
+(*)(x::XFloat32, y::XFloat32) = reinterpret(XFloat32, (*)(reinterpret(Float64, x), reinterpret(Float64, y)))
+(/)(x::XFloat32, y::XFloat32) = reinterpret(XFloat32, (/)(reinterpret(Float64, x), reinterpret(Float64, y)))
+(\)(x::XFloat32, y::XFloat32) = reinterpret(XFloat32, (\)(reinterpret(Float64, x), reinterpret(Float64, y)))
 
