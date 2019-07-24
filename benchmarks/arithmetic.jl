1fc5516338fbd67a9c21c696708c0479d86d097c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-24 17:13:27 -0400

    Update arithmetic.jl

--- benchmarks/arithmetic.jl
+++ benchmarks/arithmetic.jl
@@ -4,10 +4,10 @@ b32, b64 = Float32(cbrt(3)), Float64(cbrt(3))
 x16, x32 = XFloat16(a32), XFloat32(a64)
 y16, y32 = XFloat16(b32), XFloat32(b64)
 
-add_ab32 = @noelided @belapsed $a32 + $b32
-add_ab64 = @noelided @belapsed $a64 + $b64
-add_xy16 = @noelided @belapsed $x16 + $y16
-add_xy32 = @noelided @belapsed $x32 + $y32
+add_ab32 = @noelide @belapsed $a32 + $b32
+add_ab64 = @noelide @belapsed $a64 + $b64
+add_xy16 = @noelide @belapsed $x16 + $y16
+add_xy32 = @noelide @belapsed $x32 + $y32
 
 add_16 = string(round(add_xy16 / add_ab32, digits = 4)) 
 add_32 = string(round(add_xy32 / add_ab64, digits = 4)) 
