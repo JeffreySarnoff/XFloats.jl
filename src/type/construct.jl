e584ba7becbed493b899a29d5236eef10f9b7c62
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-24 08:34:04 -0400

    Update construct.jl

--- src/type/construct.jl
+++ src/type/construct.jl
@@ -1,23 +1,17 @@
-
-
 XFloat16(x::Float16)  = reinterpret(XFloat16, Float32(x))
-XFloat16(x::Float32)  = reinterpret(XFloat16, x)
 XFloat16(x::Float64)  = reinterpret(XFloat16, Float32(x))
 XFloat16(x::BigFloat) = reinterpret(XFloat16, Float32(x))
 
 XFloat32(x::Float16)  = reinterpret(XFloat32, Float64(x))
 XFloat32(x::Float32)  = reinterpret(XFloat32, Float64(x))
-XFloat32(x::Float64)  = reinterpret(XFloat32, x)
 XFloat32(x::BigFloat) = reinterpret(XFloat32, Float64(x))
 
 Float16(x::XFloat16)  = Float16(reinterpret(Float32, x))
-Float32(x::XFloat16)  = reinterpret(Float32, x)
 Float64(x::XFloat16)  = Float64(reinterpret(Float32, x))
 BigFloat(x::XFloat16) = BigFloat(reinterpret(Float32, x))
 
 Float16(x::XFloat32)  = Float16(reinterpret(Float64, x))
 Float32(x::XFloat32)  = Float32(reinterpret(Float64, x))
-Float64(x::XFloat32)  = reinterpret(Float64, x)
 BigFloat(x::XFloat32) = BigFloat(reinterpret(Float64, x))
 
 for T in (:Int8, :Int16, :Int32, :Int64, :Int128, :BigInt,
