4afcbcb4c685b407d7566ba01504a0abe9a7cf20
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-24 07:19:25 -0400

    Update XFloats.jl

--- src/XFloats.jl
+++ src/XFloats.jl
@@ -2,36 +2,11 @@ module XFloats
 
 export XFloat16, XFloat32
 
-import Base: hash, promote_rule, convert,
-    string, show, tryparse,
-    Float16, Float32, Float64, BigFloat,
-    UInt8, UInt16, UInt32, UInt64, UInt128,
-    Int8, Int16, Int32, Int64, Int128, BigInt
-
-import Base: precision, exponent_mask, significand_mask, eps, exponent, significand,
-    zero, one, floatmax, floatmin, nextfloat, prevfloat, frexp, ldexp, modf, decompose,
-    ==, <, <=, >=, >, !=, isless, isequal, cmp,
-    iszero, isone, isfinite, isinteger, isinf, isnan, issubnormal,
-    signbit, sign, flipsign, copysign,
-    +, -, *, /, \, abs, inv, sqrt, cbrt,
-    mod, mod1, rem, fld, fld1, div, cld, fldmod, fldmod1, divrem,
-    ceil, floor, trunc, round,
-    muladd, fma, hypot, clamp,
-    sum, prod, cumsum
-
-import Base.Math: log, log1p, log10, log2, exp, expm1, exp10, exp2,
-    sin, cos, tan, csc, sec, cot, sinpi, cospi,
-    sincos,
-    asin, acos, atan, acsc, asec, acot,
-    sinh, cosh, tanh, csch, sech, coth,
-    asinh, acosh, atanh, acsch, asech, acoth
-
 using LinearAlgebra
-import LinearAlgebra: norm, normalize, dot,
-    *, det, tr, inv, lu, qr, factorize,
-    svdvals, eigvals, eigvecs
-
 using Random
+using SpecialFunctions
+
+include("imports.jl")
 
 ## ================================================================================ ##
 
