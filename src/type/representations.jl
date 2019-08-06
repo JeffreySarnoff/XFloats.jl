1db63271221ce9a2644b747493ed810f9499386b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-08-06 16:48:38 -0400

    Update representations.jl

--- src/type/representations.jl
+++ src/type/representations.jl
@@ -1,13 +1,13 @@
-string(x::XFloat16) = string(Float16(reinterpret(Float32, x)))
-string(x::XFloat32) = string(Float32(reinterpret(Float64, x)))
+Base.string(x::XFloat16) = string(Float16(reinterpret(Float32, x)))
+Base.string(x::XFloat32) = string(Float32(reinterpret(Float64, x)))
 
-show(io::IO, x::XFloat16) = print(io, Float16(reinterpret(Float32, x)))
-show(io::IO, x::XFloat32) = print(io, Float32(reinterpret(Float64, x)))
+Base.show(io::IO, x::XFloat16) = print(io, Float16(reinterpret(Float32, x)))
+Base.show(io::IO, x::XFloat32) = print(io, Float32(reinterpret(Float64, x)))
 
-tryparse(::Type{XFloat16}, x::String) = reinterpret(XFloat16, tryparse(Float32, x))
-tryparse(::Type{XFloat32}, x::String) = reinterpret(XFloat32, tryparse(Float64, x))
+Base.tryparse(::Type{XFloat16}, x::String) = reinterpret(XFloat16, tryparse(Float32, x))
+Base.tryparse(::Type{XFloat32}, x::String) = reinterpret(XFloat32, tryparse(Float64, x))
 
-hash(x::XFloat16) = hash(reinterpret(Float32, x))
-hash(x::XFloat32) = hash(reinterpret(Float64, x))
-hash(x::XFloat16, h::UInt) = hash(Float64(reinterpret(Float32,x)), h)
-hash(x::XFloat32, h::UInt) = hash(reinterpret(Float64,x), h)
+Base.hash(x::XFloat16) = hash(reinterpret(Float32, x))
+Base.hash(x::XFloat32) = hash(reinterpret(Float64, x))
+Base.hash(x::XFloat16, h::UInt) = hash(Float64(reinterpret(Float32,x)), h)
+Base.hash(x::XFloat32, h::UInt) = hash(reinterpret(Float64,x), h)
