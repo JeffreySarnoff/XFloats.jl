cf54672c2726e8869b2066044cfc50e9bfb688d4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-08-07 07:10:03 -0400

    Update arithmetic.jl

--- benchmarks/arithmetic.jl
+++ benchmarks/arithmetic.jl
@@ -23,6 +23,22 @@ function testarith(x,y,z)
    return d
 end
 
+function testmuladd(x,y,z)
+   a = muladd(x, y, z)
+   b = muladd(z, x, y)
+   c = muladd(z, b, -y)
+   d = muladd(c, a,  x)
+   return d
+end
+
+function testfma(x,y,z)
+   a = fma(x, y, z)
+   b = fma(z, x, y)
+   c = fma(z, b, -y)
+   d = fma(c, a,  x)
+   return d
+end
+
 
 a32, a64 = Float32(pi)/7, Float64(pi)/7
 b32, b64 = Float32(cbrt(3)), Float64(cbrt(3))
@@ -57,14 +73,33 @@ relspeed_arith64 =
  round( (@noelide @belapsed testarith($x32,$y32,$z32)) /
         (@noelide @belapsed testarith($a64,$b64,$c64)), digits=2);
 
+relspeed_muladd32 =
+  round( (@noelide @belapsed testmuladd($x16,$y16,$z16)) /
+         (@noelide @belapsed testmuladd($a32,$b32,$c32)), digits=2);
+
+relspeed_muladd64 =
+ round( (@noelide @belapsed testmuladd($x32,$y32,$z32)) /
+        (@noelide @belapsed testmuladd($a64,$b64,$c64)), digits=2);
+
+relspeed_fma32 =
+  round( (@noelide @belapsed testfma($x16,$y16,$z16)) /
+         (@noelide @belapsed testfma($a32,$b32,$c32)), digits=2);
+
+relspeed_fma64 =
+ round( (@noelide @belapsed testfma($x32,$y32,$z32)) /
+        (@noelide @belapsed testfma($a64,$b64,$c64)), digits=2);
+
 
 
 relspeeds = string(
 "\n\n\trelative speeds",
-"\n\t (32)\t (64)\n\n",
-"add:   \t $relspeed_add32 \t $relspeed_add64 \n",
-"mul:   \t $relspeed_mul32 \t $relspeed_mul64 \n",
-"arith: \t $relspeed_arith32 \t $relspeed_arith64 \n");
+"\n\t  (32)\t (64)\n\n",
+"add:    \t $relspeed_add32 \t $relspeed_add64 \n",
+"mul:    \t $relspeed_mul32 \t $relspeed_mul64 \n",
+"arith:  \t $relspeed_arith32 \t $relspeed_arith64 \n",
+"muladd: \t $relspeed_muladd \t $relspeed_muladd \n",
+"fma:    \t $relspeed_fma32 \t $relspeed_fma64 \n");
 
 print(relspeeds);
-;
+
+
