f48e6475c23a90d57be5ac64b6724cc77e0615fa
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-01-31 23:27:36 -0500

    Update arithmetic.jl

--- src/dispatch/arithmetic.jl
+++ src/dispatch/arithmetic.jl
@@ -15,6 +15,9 @@ for F in (:+, :-, :*, :/, :\, :^)
     $F(x::Float64, y::XFloat32) = XFloat32($F(x, Float64(y)))
     $F(x::XFloat32, y::T) where {T<:Real} = XFloat32($F(promote(x,y)...))
     $F(x::T, y::XFloat32) where {T<:Real} = XFloat32($F(promote(x,y)...))
+
+    $F(x::XFloat16, y::XFloat32) = XFloat32($F(Float64(x), Float64(y)))
+    $F(x::XFloat32, y::XFloat16) = XFloat32($F(Float64(x), Float64(y)))
   end
 end
 
