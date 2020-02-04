904df002e137e1c28a5688ea2c789bfc0840ef14
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-02-04 18:45:32 -0500

    Update construct.jl

--- src/type/construct.jl
+++ src/type/construct.jl
@@ -1,28 +1,40 @@
-XFloat16(x::Float16)  = reinterpret(XFloat16, Float32(x))
-XFloat16(x::Float64)  = reinterpret(XFloat16, Float32(x))
-XFloat16(x::BigFloat) = reinterpret(XFloat16, Float32(x))
+XFloat32(x::XFloat16) = reinterpret(XFloat32, Float64(reinterpret(Float32,x)))
+XFloat16(x::XFloat32) = reinterpret(XFloat16, Float32(reinterpret(Float64,x)))
 
-XFloat32(x::Float16)  = reinterpret(XFloat32, Float64(x))
-XFloat32(x::Float32)  = reinterpret(XFloat32, Float64(x))
-XFloat32(x::BigFloat) = reinterpret(XFloat32, Float64(x))
+XFloat32(x::Float64) = reinterpret(XFloat32, x)
+XFloat32(x::Float32) = reinterpret(XFloat32, Float64(x))
+XFloat32(x::Float16) = reinterpret(XFloat32, Float64(x))
+XFloat32(x::Integer) = reinterpret(XFloat32, Float64(Float32(x)))
+XFloat32(x::Real) = XFloat32(Float64(x))
 
-Float16(x::XFloat16)  = Float16(reinterpret(Float32, x))
-Float64(x::XFloat16)  = Float64(reinterpret(Float32, x))
-BigFloat(x::XFloat16) = BigFloat(reinterpret(Float32, x))
+XFloat16(x::Float64) = reinterpret(XFloat16, Float32(x))
+XFloat16(x::Float32) = reinterpret(XFloat16, x)
+XFloat16(x::Float16) = reinterpret(XFloat16, Float32(x))
+XFloat16(x::Integer) = reinterpret(XFloat16, Float32(Float16(x)))
+XFloat16(x::Real) = XFloat16(Float32(x))
 
-Float16(x::XFloat32)  = Float16(reinterpret(Float64, x))
-Float32(x::XFloat32)  = Float32(reinterpret(Float64, x))
-BigFloat(x::XFloat32) = BigFloat(reinterpret(Float64, x))
+Float64(x::XFloat32) = reinterpret(Float64, x)
+Float32(x::XFloat32) = Float32(Float64(x))
+Float16(x::XFloat32) = Float16(Float64(x))
 
-for T in (:Int8, :Int16, :Int32, :Int64, :Int128, :BigInt,
-          :UInt8, :UInt16, :UInt32, :UInt64, :UInt128)
-  @eval begin
-    XFloat16(x::$T) = reinterpret(XFloat16, Float32(x))
-    XFloat32(x::$T) = reinterpret(XFloat32, Float64(x))
-    $T(x::XFloat16) = $T(reinterpret(Float32,x))
-    $T(x::XFloat32) = $T(reinterpret(Float64,x))
-  end
+Float64(x::XFloat16) = Float64(Float32(x))
+Float32(x::XFloat16) = reinterpret(Float32, x)
+Float16(x::XFloat16) = Float16(Float32(x))
+
+for I in (:Int8, :Int16, :Int32, :Int64, :Int128, :BigInt,
+          :UInt8, :UInt16, :UInt32, :UInt64, :UInt128,)
+    @eval begin
+        $I(x::XFloat32) = $I(Float64(x))
+        $I(x::XFloat16) = $I(Float32(x))
+        $I(x::XFloat16) = $I(reinterpret(Float32,x))
+        $I(x::XFloat32) = $I(reinterpret(Float64,x))
+    end
 end
+
+XFloat16(x::BigFloat) = reinterpret(XFloat16, Float32(x))
+XFloat32(x::BigFloat) = reinterpret(XFloat32, Float64(x))
+BigFloat(x::XFloat16) = BigFloat(reinterpret(Float32, x))
+BigFloat(x::XFloat32) = BigFloat(reinterpret(Float64, x))
 
 XFloat16(x::Bool) = reinterpret(XFloat16, Float32(x))
 XFloat32(x::Bool) = reinterpret(XFloat32, Float64(x))
@@ -53,4 +65,3 @@ function Base.ldexp(x::XFloat32, xp::I) where {I<:Integer}
    ldxp = ldexp(fr, xp)
    return XFloat32(ldxp)
 end
-
