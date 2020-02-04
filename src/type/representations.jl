2391378b29bf4582cbed4c319b73548cbc335f48
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-02-04 18:57:33 -0500

    Update representations.jl

--- src/type/representations.jl
+++ src/type/representations.jl
@@ -7,23 +7,23 @@ Base.show(io::IO, x::XFloat32) = print(io, Float32(reinterpret(Float64, x)))
 Base.tryparse(::Type{XFloat16}, x::String) = reinterpret(XFloat16, tryparse(Float32, x))
 Base.tryparse(::Type{XFloat32}, x::String) = reinterpret(XFloat32, tryparse(Float64, x))
 
-Base.Printf.ini_dec(d::XFloat16, ndigits, digits) =
-    Base.Printf.ini_dec(reinterpret(Float32, d), ndigits, digits)
+Printf.ini_dec(d::XFloat16, ndigits, digits) =
+    Printf.ini_dec(reinterpret(Float32, d), ndigits, digits)
 
-Base.Printf.ini_dec(d::XFloat32, ndigits, digits) =
-    Base.Printf.ini_dec(reinterpret(Float64, d), ndigits, digits)
+Printf.ini_dec(d::XFloat32, ndigits, digits) =
+    Printf.ini_dec(reinterpret(Float64, d), ndigits, digits)
 
-Base.Printf.ini_dec(d::XFloat16, ndigits::Int32, digits) =
-    Base.Printf.ini_dec(reinterpret(Float32, d), ndigits, digits)
+Printf.ini_dec(d::XFloat16, ndigits::Int32, digits) =
+    Printf.ini_dec(reinterpret(Float32, d), ndigits, digits)
 
-Base.Printf.ini_dec(d::XFloat32, ndigits::Int32, digits) =
-    Base.Printf.ini_dec(reinterpret(Float64, d), ndigits, digits)
+Printf.ini_dec(d::XFloat32, ndigits::Int32, digits) =
+    Printf.ini_dec(reinterpret(Float64, d), ndigits, digits)
 
-Base.Printf.ini_dec(d::XFloat16, ndigits::Int64, digits) =
-    Base.Printf.ini_dec(reinterpret(Float32, d), ndigits, digits)
+Printf.ini_dec(d::XFloat16, ndigits::Int64, digits) =
+    Printf.ini_dec(reinterpret(Float32, d), ndigits, digits)
 
-Base.Printf.ini_dec(d::XFloat32, ndigits::Int64, digits) =
-    Base.Printf.ini_dec(reinterpret(Float64, d), ndigits, digits)
+Printf.ini_dec(d::XFloat32, ndigits::Int64, digits) =
+    Printf.ini_dec(reinterpret(Float64, d), ndigits, digits)
 
 
 Base.hash(x::XFloat16) = hash(reinterpret(Float32, x))
